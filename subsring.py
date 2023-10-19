3ab9ab8a67bf178415e0fec5b9d18d36b241c13c
Lakshminarasimhan <lakshminarasimhan592@gmail.com>
2023-10-19 21:25:53 +0530

    fixed substring

--- subsring.py
+++ subsring.py
@@ -1,10 +1,10 @@
-s = "open-source denotes software for which the original source code is made freely available and may be redistributed and modified."
-str1 = "source"
-count = 0
-words = s.split()
-
-for word in words:
-    if word == str1:
-        count += 1
-
+s="open-source denotes software for which the original source code is made freely available and may be redistributed and modified."
+str1="source"
+count=0
+d=s.split()
+for i in range (len(d)):
+    if(d[i]==str1):
+        count=count+1
 print(count)
+
+
