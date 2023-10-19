c509e8cf7d0fcc8908ae4c1e590ac2e2defe0fe8
kishore-rk <kishorev2425@gmail.com>
2023-10-19 14:57:07 +0530

    fixed the 4 issues

--- subsring.py
+++ subsring.py
@@ -1,8 +1,9 @@
-s="open-source denotes software for which the original source code is made freely available and may be redistributed and modified."
+s="open-source denotes software for which the original source code is made freely available and maysource be redistributed and modified."
 str1="source"
+n = len(str1)
 count=0
 d=s.split(",")
-for i in range(len(s)):
-    if(d[i]==stri):
+for i in range(len(s)-n):
+    if(s[i:i+n]==str1):
         count=count+1
 print(count)
