cb465bcb1bbfd70a1525939b77745fd376dc1503
Lakshminarasimhan <lakshminarasimhan592@gmail.com>
2023-10-19 21:08:49 +0530

    fixed occurence

--- Number_of_occurance.py
+++ Number_of_occurance.py
@@ -1,7 +1,12 @@
 #code to find the no of occurances of a number in an array
-a=[10,20,30,40,50,20]
-c=20
+a = [10, 20, 30, 40, 50, 20]
+c = 20
+count = 0
+
 for i in range(len(a)):
-    if(a[i]==c):
-        print(1)
+    if a[i] == c:
+        count += 1
+
+print(count)
+
         
